d85398c6530301df0a24a8aa1703d987baed7fb6
Jullis23 <jullis.areias@gmail.com>
2026-06-05 17:08:21 -0300

    Add print statement for direct GitHub changes warning

--- Server.java
+++ Server.java
@@ -1,3 +1,4 @@
 public class Server{
 	public static void main(String[] args) {}
+	System.out.println("Teste de alteração feito diretamente no GitHub, ATENÇÃO: Não é recomendado, o ideal e fazer alterações externamente");
 }
